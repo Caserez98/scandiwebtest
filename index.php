1360966c5a306bd43623595a92e9d8a03c47fb2b
Alexander Caserez <46090778+Caserez98@users.noreply.github.com>
2022-01-19 19:15:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ switch ($request) {
         break;
     default:
         http_response_code(404);
-        require __DIR__ . './views/404.php';
+        require __DIR__ . '/views/404.php';
         break;
 }
 ?>
